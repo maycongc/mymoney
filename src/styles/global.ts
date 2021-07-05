d55f21ade2009fc2debb6d3a42ba20fce224693c
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-07-04 23:51:03 -0300

    refactor: global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,9 +8,23 @@ export const GlobalStyles = createGlobalStyle`
 
   }
 
+  html {
+    font-size: 62.5%;
+  }
+
   body {
     background: ${p => p.theme.background};
     -webkit-font-smoothing: antialiased;
+
+    font-size: 1.6rem;
+
+    @media (max-width: 1080px) {
+      font-size: 1.5rem;
+    }
+
+    @media (max-with: 720px) {
+      font-size: 1.4rem;
+    }
   }
 
   html, input, textarea, button {
@@ -20,18 +34,6 @@ export const GlobalStyles = createGlobalStyle`
 
   h1, h2, h3, h4, h5, h6, strong {
     font-weight: 600;
-  }
-
-  html {
-    font-size: 62.5%;
-
-    @media (max-width: 1080px) {
-      font-size: 1.5rem;
-    }
-
-    @media (max-with: 720px) {
-      font-size: 1.4rem;
-    }
   }
 
   button {
