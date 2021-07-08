ca17507fae1d21b3898a3df6e229457d7fec4397
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-07-08 20:19:46 -0300

    feat: add modal to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
+
 import { Header } from '../../components/Header';
 import { Dashboard } from '../../components/Dashboard';
+import { NewTransactionModal } from '../../components/NewTransactionModal';
 
 export function Home(): JSX.Element {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
     </>
   );
 }
