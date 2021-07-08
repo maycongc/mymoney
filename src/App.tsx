dff9d4755458c5a9c8e5ea252d046962950f435a
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-07-08 20:21:32 -0300

    feat: add context to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import { Routes } from './Routes';
+
 import { ThemeContextProvider } from './contexts/ThemeContext';
+import { TransactionsContextProvider } from './contexts/TransactionsContext';
+
 import { GlobalStyles } from './styles/global';
 
 export function App(): JSX.Element {
   return (
     <BrowserRouter>
       <ThemeContextProvider>
-        <GlobalStyles />
-        <Routes />
+        <TransactionsContextProvider>
+          <GlobalStyles />
+          <Routes />
+        </TransactionsContextProvider>
       </ThemeContextProvider>
     </BrowserRouter>
   );
