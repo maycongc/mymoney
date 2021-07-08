6006f0a20753d7ed8737c7620220370bd9cd97ca
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-07-08 20:16:30 -0300

    feat: add button functionality

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,21 @@ import logoImg from '../../assets/images/logo.svg';
 
 import * as S from './styles';
 
-export function Header(): JSX.Element {
+type HeaderProps = {
+  onOpenNewTransactionModal: () => void;
+};
+
+export function Header({
+  onOpenNewTransactionModal,
+}: HeaderProps): JSX.Element {
   return (
     <S.HeaderWrapper>
       <S.Content>
         <img src={logoImg} alt="my money" />
 
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </S.Content>
     </S.HeaderWrapper>
   );
